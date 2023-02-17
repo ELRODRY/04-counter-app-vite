bdced70d2dff8ba8d1fd5f3e58042d9459e24a2c
ELRODRY <rodrigoezequielolivaresvcp@gmail.com>
2023-02-17 15:30:21 -0300

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'; // elemento para renderizar aplicaion
+import React from 'react'; // importacion de react
 import ReactDOM from 'react-dom/client'; // elemento para renderizar aplicaion
 
 function App() {
   return <h1> Hola Mundo</h1>;
 }
 
+// Forma de renderizar en el DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
